8cd2d90de042a99c497f75eee7c8211fe35599c0
Megh Shetty <meghvshetty@outlook.com>
2024-06-22 10:31:47 +0100

    create a Jira.go package to handle jira api tasks

---
+++ api/jira/jira-api.go
@@ -0,0 +1,7 @@
+/*
+===================================================================================================
+JIRA API Package
+Jira REST API doc https://developer.atlassian.com/cloud/jira/platform/basic-auth-for-rest-apis/
+===================================================================================================
+*/
+package jira
