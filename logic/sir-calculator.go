935639f9a0ae29d0d7800cf8bbcf2d48d715e9a2
Megh Shetty <meghvshetty@outlook.com>
2024-07-05 15:36:29 +0100

    input struct added

---
+++ logic/sir-calculator.go
@@ -0,0 +1,12 @@
+package logic
+
+type TriageInput struct {
+	DataClassification      string
+	MultipleReleaseOveryear string
+	IntegrityRating         string
+	NewInfrastructure       string
+	NewConnectivity         string
+	RegulatoryRequirement   string
+	UserImpact              string
+	PatternUsed             string
+}
