97675a53cda7e66a3d831dce3cbd7c9c5738ef65
Megh Shetty <meghvshetty@outlook.com>
2024-07-08 13:56:45 +0100

    create a logic for SIR cal

--- logic/sir-calculator.go
+++ logic/sir-calculator.go
@@ -10,3 +10,20 @@ type TriageInput struct {
 	UserImpact              string
 	PatternUsed             string
 }
+
+func SirCalculator(input *TriageInput) string {
+	var sir string = "0"
+
+	if ((input.DataClassification == "Highly Confidential" || input.DataClassification == "Confidential") && (input.IntegrityRating == "Severe" || input.IntegrityRating == "High") && input.MultipleReleaseOveryear == "Yes") && (input.PatternUsed == "Hybrid" ||
+		input.PatternUsed == "New") {
+		sir = "3"
+	} else if (input.NewInfrastructure == "Yes" || input.NewConnectivity == "Yes" && input.MultipleReleaseOveryear == "Yes") && (input.PatternUsed == "Hybrid" || input.PatternUsed == "New") {
+		sir = "2"
+	} else if (input.NewInfrastructure == "Yes" || input.NewConnectivity == "Yes" || input.RegulatoryRequirement == "Yes" || input.UserImpact == ">10,000") && (input.PatternUsed == "Hybrid" || input.PatternUsed == "New") {
+		sir = "1"
+	} else {
+		sir = "0"
+	}
+
+	return sir
+}
