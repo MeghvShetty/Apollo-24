a1a89fcfde0c2d1e609e47dbcecf5e2d69ea7650
Megh Shetty <meghvshetty@outlook.com>
2024-07-10 14:52:36 +0100

    added templates dir

--- router/router.go
+++ router/router.go
@@ -6,6 +6,7 @@ package router
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,8 +14,9 @@ import (
 func WebServer() {
 	router := gin.Default()
 
+	router.LoadHTMLGlob("templates/*")
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.File("static files/index.html")
+		ctx.HTML(http.StatusOK, "index.html", nil)
 	})
 	log.Fatal(router.Run(":3000"))
 }
