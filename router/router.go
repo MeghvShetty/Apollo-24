f7412e3349b1f1499745ae82d364de2418e6be6e
Megh Shetty <meghvshetty@outlook.com>
2024-07-10 15:44:15 +0100

    create post form

--- router/router.go
+++ router/router.go
@@ -5,9 +5,11 @@ Apollo-24 routing handler
 package router
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/The-Manchester-Project/Apollo-24/logic"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,5 +20,23 @@ func WebServer() {
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "index.html", nil)
 	})
+
+	router.POST("/submit-form", func(ctx *gin.Context) {
+		var triageInput logic.TriageInput
+		if err := ctx.ShouldBind(&triageInput); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		fmt.Println(logic.SirCalculator(&triageInput))
+	})
+
+	// router.POST("/submit-form", func(ctx *gin.Context) {
+	// 	var triageInput logic.TriageInput
+	// 	if err := ctx.ShouldBind(&triageInput); err != nil {
+	// 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	// 		return
+	// 	}
+
+	// })
 	log.Fatal(router.Run(":3000"))
 }
