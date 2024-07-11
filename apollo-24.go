2188443d7838fcb1f733607aadf89c8c0dadf93c
Megh Shetty <meghvshetty@outlook.com>
2024-07-11 17:12:02 +0100

    removed the comments from main file

--- apollo-24.go
+++ apollo-24.go
@@ -5,34 +5,6 @@ import (
 )
 
 func main() {
-	// NewIssue := &jira.IssueTemplate{
-	// 	SINumber:              "JB007",
-	// 	SILink:                "google.com/hjhd/jskhd/dh",
-	// 	BIARecord:             "facebook.xom",
-	// 	ProjectOverview:       "Man is a man and only a man will know what a man is !!",
-	// 	PlatformName:          "Value added",
-	// 	LabName:               "Smarty pants",
-	// 	GW1Date:               "2024-12-08",
-	// 	SolutionArchitectName: "Megh Shetty",
-	// 	ProjectName:           "Template value",
-	// 	SIRrating:             []string{"SIRC"},
-	// 	DataClassification:    "Confidential",
-	// }
-
-	// jira.CreateIssue(NewIssue)
-
-	// Input := &logic.TriageInput{
-	// 	DataClassification:      "Confidential",
-	// 	MultipleReleaseOveryear: "Yes",
-	// 	IntegrityRating:         "High",
-	// 	NewInfrastructure:       "Yes",
-	// 	NewConnectivity:         "No",
-	// 	RegulatoryRequirement:   "Yes",
-	// 	UserImpact:              ">10,000",
-	// 	PatternUsed:             "New",
-	// }
-
-	// fmt.Println(logic.SirCalculator(Input))
 
 	router.WebServer()
 }
