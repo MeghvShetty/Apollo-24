8423555d6658c27b61495e3aea71258d75df9f3a
Megh Shetty <meghvshetty@outlook.com>
2024-07-13 19:53:03 +0100

    api testing

--- apollo-24.go
+++ apollo-24.go
@@ -1,10 +1,21 @@
 package main
 
-import (
-	"github.com/The-Manchester-Project/Apollo-24/router"
-)
+import "github.com/The-Manchester-Project/Apollo-24/api/jira"
 
 func main() {
+	NewIssue := &jira.IssueTemplate{
+		SINumber:              "JB007",
+		SILink:                "google.com/hjhd/jskhd/dh",
+		BIARecord:             "facebook.xom",
+		ProjectOverview:       "Man is a man and only a man will know what a man is !!",
+		PlatformName:          "Value added",
+		LabName:               "Smarty pants",
+		GW1Date:               "2024-12-08",
+		SolutionArchitectName: "Megh Shetty",
+		ProjectName:           "Template value",
+		SIRrating:             []string{"SIRC"},
+		DataClassification:    "Confidential",
+	}
 
-	router.WebServer()
+	jira.CreateIssue(NewIssue)
 }
