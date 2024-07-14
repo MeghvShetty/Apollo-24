00079628b038b3af756b9ed782c58ec54ee9c805
Megh Shetty <meghvshetty@outlook.com>
2024-07-14 17:50:41 +0100

    added webserver

--- apollo-24.go
+++ apollo-24.go
@@ -1,25 +1,22 @@
 package main
 
-import (
-	"github.com/The-Manchester-Project/Apollo-24/api/jira"
-	"github.com/The-Manchester-Project/Apollo-24/router"
-)
+import "github.com/The-Manchester-Project/Apollo-24/router"
 
 func main() {
-	NewIssue := &jira.IssueTemplate{
-		SINumber:              "JB007",
-		SILink:                "google.com/hjhd/jskhd/dh",
-		BIARecord:             "facebook.xom",
-		ProjectOverview:       "Man is a man and only a man will know what a man is !!",
-		PlatformName:          "Value added",
-		LabName:               "Smarty pants",
-		GW1Date:               "2024-12-08",
-		SolutionArchitectName: "Megh Shetty",
-		ProjectName:           "Template value",
-		SIRrating:             []string{"SIRC"},
-		DataClassification:    "Confidential",
-	}
+	// NewIssue := &jira.IssueTemplate{
+	// 	SINumber:              "JB007",
+	// 	SILink:                "google.com/hjhd/jskhd/dh",
+	// 	BIARecord:             "facebook.xom",
+	// 	ProjectOverview:       "Man is a man and only a man will know what a man is !!",
+	// 	PlatformName:          "Value added",
+	// 	LabName:               "Smarty pants",
+	// 	GW1Date:               "2024-12-08",
+	// 	SolutionArchitectName: "Megh Shetty",
+	// 	ProjectName:           "Template value",
+	// 	SIRrating:             []string{"SIRC"},
+	// 	DataClassification:    "Confidential",
+	// }
 
-	jira.CreateIssue(NewIssue)
+	// fmt.Println(jira.CreateIssue(NewIssue))
 	router.WebServer()
 }
