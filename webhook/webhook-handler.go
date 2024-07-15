2717c13e05f53d115ac9ea05a8f058f9efcdaa7c
Megh Shetty <meghvshetty@outlook.com>
2024-07-15 20:32:09 +0100

    getting all the info from webhook

--- webhook/webhook-handler.go
+++ webhook/webhook-handler.go
@@ -8,35 +8,147 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// WebhookPayload represents the JSON structure of the webhook payload
 type WebhookPayload struct {
-	Issue Issue `json:"issue"`
+	Timestamp          int64     `json:"timestamp"`
+	WebhookEvent       string    `json:"webhookEvent"`
+	IssueEventTypeName string    `json:"issue_event_type_name"`
+	User               User      `json:"user"`
+	Issue              Issue     `json:"issue"`
+	Changelog          Changelog `json:"changelog"`
 }
 
-// Issue represents the 'issue' field in the webhook payload
+type User struct {
+	Self        string     `json:"self"`
+	AccountID   string     `json:"accountId"`
+	AvatarUrls  AvatarUrls `json:"avatarUrls"`
+	DisplayName string     `json:"displayName"`
+	Active      bool       `json:"active"`
+	TimeZone    string     `json:"timeZone"`
+	AccountType string     `json:"accountType"`
+}
+
+type AvatarUrls struct {
+	Size48x48 string `json:"48x48"`
+	Size24x24 string `json:"24x24"`
+	Size16x16 string `json:"16x16"`
+	Size32x32 string `json:"32x32"`
+}
+
 type Issue struct {
+	ID     string `json:"id"`
+	Self   string `json:"self"`
 	Key    string `json:"key"`
 	Fields Fields `json:"fields"`
 }
 
-// Fields represents the 'fields' field within the Issue in the webhook payload
 type Fields struct {
-	Summary       string   `json:"summary"`
-	Assignee      Assignee `json:"assignee"`
-	CustomField34 string   `json:"customfield_10034"`
-	Description   string   `json:"description"`
-	Labels        []string `json:"labels"`
+	StatusCategoryChangeDate string      `json:"statuscategorychangedate"`
+	IssueType                IssueType   `json:"issuetype"`
+	TimeSpent                interface{} `json:"timespent"`
+	Project                  Project     `json:"project"`
+	CustomField10034         string      `json:"customfield_10034"`
+	AggregateTimeSpent       interface{} `json:"aggregatetimespent"`
+	Resolution               Resolution  `json:"resolution"`
+	ResolutionDate           string      `json:"resolutiondate"`
+	WorkRatio                int         `json:"workratio"`
+	LastViewed               string      `json:"lastViewed"`
+	Watches                  Watches     `json:"watches"`
+	Created                  string      `json:"created"`
+	Priority                 Priority    `json:"priority"`
+	Labels                   []string    `json:"labels"`
+	Assignee                 Assignee    `json:"assignee"`
+	Updated                  string      `json:"updated"`
+	Status                   Status      `json:"status"`
+	Description              string      `json:"description"`
+	Summary                  string      `json:"summary"`
+	Creator                  User        `json:"creator"`
+	Reporter                 User        `json:"reporter"`
+}
+
+type IssueType struct {
+	Self        string `json:"self"`
+	ID          string `json:"id"`
+	Description string `json:"description"`
+	IconUrl     string `json:"iconUrl"`
+	Name        string `json:"name"`
+	Subtask     bool   `json:"subtask"`
+}
+
+type Project struct {
+	Self           string     `json:"self"`
+	ID             string     `json:"id"`
+	Key            string     `json:"key"`
+	Name           string     `json:"name"`
+	ProjectTypeKey string     `json:"projectTypeKey"`
+	Simplified     bool       `json:"simplified"`
+	AvatarUrls     AvatarUrls `json:"avatarUrls"`
+}
+
+type Resolution struct {
+	Self        string `json:"self"`
+	ID          string `json:"id"`
+	Description string `json:"description"`
+	Name        string `json:"name"`
+}
+
+type Watches struct {
+	Self       string `json:"self"`
+	WatchCount int    `json:"watchCount"`
+	IsWatching bool   `json:"isWatching"`
+}
+
+type Priority struct {
+	Self    string `json:"self"`
+	IconUrl string `json:"iconUrl"`
+	Name    string `json:"name"`
+	ID      string `json:"id"`
 }
 
 type Assignee struct {
-	DisplayName string `json:"displayName"`
+	Self        string     `json:"self"`
+	AccountID   string     `json:"accountId"`
+	AvatarUrls  AvatarUrls `json:"avatarUrls"`
+	DisplayName string     `json:"displayName"`
+	Active      bool       `json:"active"`
+	TimeZone    string     `json:"timeZone"`
+	AccountType string     `json:"accountType"`
 }
 
-// WebhookHandler handles incoming webhook requests
+type Status struct {
+	Self           string         `json:"self"`
+	Description    string         `json:"description"`
+	IconUrl        string         `json:"iconUrl"`
+	Name           string         `json:"name"`
+	ID             string         `json:"id"`
+	StatusCategory StatusCategory `json:"statusCategory"`
+}
+
+type StatusCategory struct {
+	Self      string `json:"self"`
+	ID        int    `json:"id"`
+	Key       string `json:"key"`
+	ColorName string `json:"colorName"`
+	Name      string `json:"name"`
+}
+
+type Changelog struct {
+	ID    string       `json:"id"`
+	Items []ChangeItem `json:"items"`
+}
+
+type ChangeItem struct {
+	Field      string      `json:"field"`
+	FieldType  string      `json:"fieldtype"`
+	FieldID    string      `json:"fieldId"`
+	From       interface{} `json:"from"`
+	FromString interface{} `json:"fromString"`
+	To         interface{} `json:"to"`
+	ToString   string      `json:"toString"`
+}
+
 func WebhookHandler(c *gin.Context) {
 	var payload WebhookPayload
 
-	// Bind JSON payload to WebhookPayload struct
 	if err := c.ShouldBindJSON(&payload); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
 		return
@@ -48,13 +160,9 @@ func WebhookHandler(c *gin.Context) {
 
 	// Process the webhook payload as needed (e.g., save to database, trigger actions, etc.)
 
-	// Respond to the webhook request
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Webhook received and processed successfully",
-	})
+	c.JSON(http.StatusOK, gin.H{"message": "Webhook received and processed successfully"})
 }
 
-// prettyPrintJSON prints JSON in an indented format
 func prettyPrintJSON(v interface{}) {
 	b, err := json.MarshalIndent(v, "", "  ")
 	if err != nil {
