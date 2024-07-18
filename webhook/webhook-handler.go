ffd7a46b2a16a2c7fa375b58227d19ff25fccc95
Megh Shetty <meghvshetty@outlook.com>
2024-07-18 17:29:48 +0100

    getting raw data

--- webhook/webhook-handler.go
+++ webhook/webhook-handler.go
@@ -1,6 +1,7 @@
 package webhook
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -155,7 +156,7 @@ func WebhookHandler(c *gin.Context) {
 
 	// Print the received payload for debugging
 	fmt.Println("Received Webhook Payload:")
-	// prettyPrintJSON(payload)
+	prettyPrintJSON(payload)
 
 	// Process the webhook payload as needed (e.g., save to database, trigger actions, etc.)
 	var assignee string = payload.User.DisplayName
@@ -167,11 +168,11 @@ func WebhookHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Webhook received and processed successfully"})
 }
 
-// func prettyPrintJSON(v interface{}) {
-// 	b, err := json.MarshalIndent(v, "", "  ")
-// 	if err != nil {
-// 		fmt.Println("Error formatting JSON:", err)
-// 		return
-// 	}
-// 	fmt.Println(string(b))
-// }
+func prettyPrintJSON(v interface{}) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		fmt.Println("Error formatting JSON:", err)
+		return
+	}
+	fmt.Println(string(b))
+}
