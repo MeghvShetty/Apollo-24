625659429fcad74a7094d826246ef8b42192b5ec
Megh Shetty <meghvshetty@outlook.com>
2024-07-18 16:58:38 +0100

    data fittering

--- webhook/webhook-handler.go
+++ webhook/webhook-handler.go
@@ -9,30 +9,30 @@ import (
 )
 
 type WebhookPayload struct {
-	Timestamp          int64     `json:"timestamp"`
-	WebhookEvent       string    `json:"webhookEvent"`
-	IssueEventTypeName string    `json:"issue_event_type_name"`
-	User               User      `json:"user"`
-	Issue              Issue     `json:"issue"`
-	Changelog          Changelog `json:"changelog"`
+	Timestamp          int64  `json:"timestamp"`
+	WebhookEvent       string `json:"webhookEvent"`
+	IssueEventTypeName string `json:"issue_event_type_name"`
+	User               User   `json:"user"`
+	Issue              Issue  `json:"issue"`
+	// Changelog Changelog `json:"changelog"`
 }
 
 type User struct {
-	Self        string     `json:"self"`
-	AccountID   string     `json:"accountId"`
-	AvatarUrls  AvatarUrls `json:"avatarUrls"`
-	DisplayName string     `json:"displayName"`
-	Active      bool       `json:"active"`
-	TimeZone    string     `json:"timeZone"`
-	AccountType string     `json:"accountType"`
+	Self      string `json:"self"`
+	AccountID string `json:"accountId"`
+	// AvatarUrls  AvatarUrls `json:"avatarUrls"`
+	DisplayName string `json:"displayName"`
+	Active      bool   `json:"active"`
+	TimeZone    string `json:"timeZone"`
+	AccountType string `json:"accountType"`
 }
 
-type AvatarUrls struct {
-	Size48x48 string `json:"48x48"`
-	Size24x24 string `json:"24x24"`
-	Size16x16 string `json:"16x16"`
-	Size32x32 string `json:"32x32"`
-}
+// type AvatarUrls struct {
+// 	Size48x48 string `json:"48x48"`
+// 	Size24x24 string `json:"24x24"`
+// 	Size16x16 string `json:"16x16"`
+// 	Size32x32 string `json:"32x32"`
+// }
 
 type Issue struct {
 	ID     string `json:"id"`
@@ -45,24 +45,24 @@ type Fields struct {
 	StatusCategoryChangeDate string      `json:"statuscategorychangedate"`
 	IssueType                IssueType   `json:"issuetype"`
 	TimeSpent                interface{} `json:"timespent"`
-	Project                  Project     `json:"project"`
-	CustomField10034         string      `json:"customfield_10034"`
-	AggregateTimeSpent       interface{} `json:"aggregatetimespent"`
-	Resolution               Resolution  `json:"resolution"`
-	ResolutionDate           string      `json:"resolutiondate"`
-	WorkRatio                int         `json:"workratio"`
-	LastViewed               string      `json:"lastViewed"`
-	Watches                  Watches     `json:"watches"`
-	Created                  string      `json:"created"`
-	Priority                 Priority    `json:"priority"`
-	Labels                   []string    `json:"labels"`
-	Assignee                 Assignee    `json:"assignee"`
-	Updated                  string      `json:"updated"`
-	Status                   Status      `json:"status"`
-	Description              string      `json:"description"`
-	Summary                  string      `json:"summary"`
-	Creator                  User        `json:"creator"`
-	Reporter                 User        `json:"reporter"`
+	// Project                  Project     `json:"project"`
+	CustomField10034   string      `json:"customfield_10034"`
+	AggregateTimeSpent interface{} `json:"aggregatetimespent"`
+	Resolution         Resolution  `json:"resolution"`
+	ResolutionDate     string      `json:"resolutiondate"`
+	WorkRatio          int         `json:"workratio"`
+	LastViewed         string      `json:"lastViewed"`
+	// Watches            Watches     `json:"watches"`
+	Created string `json:"created"`
+	// Priority           Priority    `json:"priority"`
+	Labels      []string `json:"labels"`
+	Assignee    Assignee `json:"assignee"`
+	Updated     string   `json:"updated"`
+	Status      Status   `json:"status"`
+	Description string   `json:"description"`
+	Summary     string   `json:"summary"`
+	Creator     User     `json:"creator"`
+	Reporter    User     `json:"reporter"`
 }
 
 type IssueType struct {
@@ -75,13 +75,13 @@ type IssueType struct {
 }
 
 type Project struct {
-	Self           string     `json:"self"`
-	ID             string     `json:"id"`
-	Key            string     `json:"key"`
-	Name           string     `json:"name"`
-	ProjectTypeKey string     `json:"projectTypeKey"`
-	Simplified     bool       `json:"simplified"`
-	AvatarUrls     AvatarUrls `json:"avatarUrls"`
+	Self           string `json:"self"`
+	ID             string `json:"id"`
+	Key            string `json:"key"`
+	Name           string `json:"name"`
+	ProjectTypeKey string `json:"projectTypeKey"`
+	Simplified     bool   `json:"simplified"`
+	// AvatarUrls     AvatarUrls `json:"avatarUrls"`
 }
 
 type Resolution struct {
@@ -91,60 +91,60 @@ type Resolution struct {
 	Name        string `json:"name"`
 }
 
-type Watches struct {
-	Self       string `json:"self"`
-	WatchCount int    `json:"watchCount"`
-	IsWatching bool   `json:"isWatching"`
-}
+// type Watches struct {
+// 	Self       string `json:"self"`
+// 	WatchCount int    `json:"watchCount"`
+// 	IsWatching bool   `json:"isWatching"`
+// }
 
-type Priority struct {
-	Self    string `json:"self"`
-	IconUrl string `json:"iconUrl"`
-	Name    string `json:"name"`
-	ID      string `json:"id"`
-}
+// type Priority struct {
+// 	Self    string `json:"self"`
+// 	IconUrl string `json:"iconUrl"`
+// 	Name    string `json:"name"`
+// 	ID      string `json:"id"`
+// }
 
 type Assignee struct {
-	Self        string     `json:"self"`
-	AccountID   string     `json:"accountId"`
-	AvatarUrls  AvatarUrls `json:"avatarUrls"`
-	DisplayName string     `json:"displayName"`
-	Active      bool       `json:"active"`
-	TimeZone    string     `json:"timeZone"`
-	AccountType string     `json:"accountType"`
+	Self      string `json:"self"`
+	AccountID string `json:"accountId"`
+	// AvatarUrls  AvatarUrls `json:"avatarUrls"`
+	DisplayName string `json:"displayName"`
+	Active      bool   `json:"active"`
+	TimeZone    string `json:"timeZone"`
+	AccountType string `json:"accountType"`
 }
 
 type Status struct {
-	Self           string         `json:"self"`
-	Description    string         `json:"description"`
-	IconUrl        string         `json:"iconUrl"`
-	Name           string         `json:"name"`
-	ID             string         `json:"id"`
-	StatusCategory StatusCategory `json:"statusCategory"`
+	Self        string `json:"self"`
+	Description string `json:"description"`
+	IconUrl     string `json:"iconUrl"`
+	Name        string `json:"name"`
+	ID          string `json:"id"`
+	// StatusCategory StatusCategory `json:"statusCategory"`
 }
 
-type StatusCategory struct {
-	Self      string `json:"self"`
-	ID        int    `json:"id"`
-	Key       string `json:"key"`
-	ColorName string `json:"colorName"`
-	Name      string `json:"name"`
-}
+// type StatusCategory struct {
+// 	Self      string `json:"self"`
+// 	ID        int    `json:"id"`
+// 	Key       string `json:"key"`
+// 	ColorName string `json:"colorName"`
+// 	Name      string `json:"name"`
+// }
 
-type Changelog struct {
-	ID    string       `json:"id"`
-	Items []ChangeItem `json:"items"`
-}
+// type Changelog struct {
+// 	ID    string       `json:"id"`
+// 	Items []ChangeItem `json:"items"`
+// }
 
-type ChangeItem struct {
-	Field      string      `json:"field"`
-	FieldType  string      `json:"fieldtype"`
-	FieldID    string      `json:"fieldId"`
-	From       interface{} `json:"from"`
-	FromString interface{} `json:"fromString"`
-	To         interface{} `json:"to"`
-	ToString   string      `json:"toString"`
-}
+// type ChangeItem struct {
+// 	Field      string      `json:"field"`
+// 	FieldType  string      `json:"fieldtype"`
+// 	FieldID    string      `json:"fieldId"`
+// 	From       interface{} `json:"from"`
+// 	FromString interface{} `json:"fromString"`
+// 	To         interface{} `json:"to"`
+// 	ToString   string      `json:"toString"`
+// }
 
 func WebhookHandler(c *gin.Context) {
 	var payload WebhookPayload
